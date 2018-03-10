13e96e81e6e489a5e47bd73a2388313d9721f0bd
volvoxc90 <volvo_xc90_vip@163.com>
2018-03-10 15:21:03 +0800

    注释

--- src/main/java/cn/example/SpringbootFirstDemoApplication.java
+++ src/main/java/cn/example/SpringbootFirstDemoApplication.java
@@ -3,10 +3,11 @@ package cn.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "cn.example.*")
+//@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
 public class SpringbootFirstDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootFirstDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootFirstDemoApplication.class, args);
+    }
 }
