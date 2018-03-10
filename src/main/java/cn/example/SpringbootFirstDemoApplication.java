104667d42332ee30e93bc964f4962613ba69859c
volvoxc90 <volvo_xc90_vip@163.com>
2018-03-10 15:38:18 +0800

    addMybatis

--- src/main/java/cn/example/SpringbootFirstDemoApplication.java
+++ src/main/java/cn/example/SpringbootFirstDemoApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "cn.example.*")
-//@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
 public class SpringbootFirstDemoApplication {
 
     public static void main(String[] args) {
