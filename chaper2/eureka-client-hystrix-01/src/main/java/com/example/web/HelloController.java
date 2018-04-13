a709968ba93dca3fc5eeefec8ace9e947c7bd99f
volvoxc90 <volvo_xc90_vip@163.com>
2018-04-13 14:27:08 +0800

    modify

--- chaper2/eureka-client-hystrix-01/src/main/java/com/example/web/HelloController.java
+++ chaper2/eureka-client-hystrix-01/src/main/java/com/example/web/HelloController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 /**
  * @author zhugp
  * @create 2018-04-04 14:36
@@ -16,8 +19,18 @@ public class HelloController {
     @Value("${server.port}")
     String port;
 
-    @RequestMapping(value = "/home")
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
     public String sayHello(@RequestParam(value = "name") String name) {
+
+
         return "hello, " + name + ", " + port;
+    }
+
+    public static void main(String[] args) {
+        Collection collection = new ArrayList();
+        for (int i = 0; i < 9; i++) {
+            collection.add("宝马x" + i);
+        }
+        System.out.println(collection);
     }
 }
